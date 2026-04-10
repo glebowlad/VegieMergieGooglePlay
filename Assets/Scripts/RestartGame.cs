b7d24f68560a83a16f3120a54bca0756d483a605
Glebov Vlad <glebowladic@gmail.com>
2026-04-10 09:19:06 +0300

    починил рестарт , но он теперь зависит от паузы

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -4,12 +4,14 @@ using UnityEngine.SceneManagement;
 public class RestartGame : MonoBehaviour
 {
     [SerializeField] private BestScore bestScore;
-    
+    [SerializeField] private PauseManager pauseManager;
     public void Restart()
     {
         if (bestScore != null) { bestScore.CheckBestScore(); }
         AdsManager.Instance.ShowAdOnRestart();
         SceneManager.LoadScene(0);
+        pauseManager.ResumeGame();
+
         
     }
 }
