479bd4260835dfcb4f290bc65ed57ec83b43126d
Glebov Vlad <glebowladic@gmail.com>
2026-04-14 20:10:09 +0300

    пофиксил сохранение счета при рестарте

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -5,10 +5,13 @@ public class RestartGame : MonoBehaviour
 {
     [SerializeField] private BestScore bestScore;
     [SerializeField] private PauseManager pauseManager;
+    [SerializeField] public SaveManager saveManager;
     public void Restart()
     {
         if (bestScore != null) { bestScore.CheckBestScore(); }
         AdsManager.Instance.ShowAdOnRestart();
+        Counter.totalScore = 0;
+        saveManager.SaveGame();
         SceneManager.LoadScene(0);
         pauseManager.ResumeGame();
 
